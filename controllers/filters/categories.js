19d2fd6d038555dc498e57b8100ddf801fd3e1d1
Vitalii Dimov <113688545+Carriedbydog@users.noreply.github.com>
2023-12-18 12:58:14 +0200

    Bugfix/glasses-categories
    Fixed nested response for glasses and categories, hide _id for better frontend usage.

--- controllers/filters/categories.js
+++ controllers/filters/categories.js
@@ -2,10 +2,11 @@ const Filters = require("../../models/filters");
 
 const listCategories = async (req, res) => {
   try {
-    const categories = await Filters.find({ categories: { $exists: true } });
-    res.json({
-      categories,
-    });
+    const categories = await Filters.find(
+      { categories: { $exists: true } },
+      "-_id"
+    );
+    res.json(...categories);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
--- controllers/filters/glasses.js
+++ controllers/filters/glasses.js
@@ -2,10 +2,8 @@ const Filters = require("../../models/filters");
 
 const listGlasses = async (req, res) => {
   try {
-    const glasses = await Filters.find({ glasses: { $exists: true } });
-    res.json({
-      glasses,
-    });
+    const glasses = await Filters.find({ glasses: { $exists: true } }, "-_id");
+    res.json(...glasses);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
