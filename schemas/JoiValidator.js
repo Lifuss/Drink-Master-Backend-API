c63d5add13d7608c13cafc51089a6d1d3e99c829
DianaKryzhanivska <di_parkhomenko@ukr.net>
2023-12-18 14:01:53 +0200

    constants moved

--- schemas/JoiValidator.js
+++ schemas/JoiValidator.js
@@ -2,54 +2,7 @@ const Joi = require("joi");
 
 const schemaPut = Joi.object({});
 
-const categoryTypes = [
-  "Ordinary Drink",
-  "Cocktail",
-  "Shake",
-  "Other/Unknown",
-  "Cocoa",
-  "Shot",
-  "Coffee/Tea",
-  "Homemade Liqueur",
-  "Punch/Party Drink",
-  "Beer",
-  "Soft Drink",
-];
-
-const glassTypes = [
-  "Highball glass",
-  "Cocktail glass",
-  "Old-fashioned glass",
-  "Whiskey Glass",
-  "Collins glass",
-  "Pousse cafe glass",
-  "Champagne flute",
-  "Whiskey sour glass",
-  "Cordial glass",
-  "Brandy snifter",
-  "White wine glass",
-  "Nick and Nora Glass",
-  "Hurricane glass",
-  "Coffee mug",
-  "Shot glass",
-  "Jar",
-  "Irish coffee cup",
-  "Punch bowl",
-  "Pitcher",
-  "Pint glass",
-  "Copper Mug",
-  "Wine Glass",
-  "Beer mug",
-  "Margarita/Coupette glass",
-  "Beer pilsner",
-  "Beer Glass",
-  "Parfait glass",
-  "Mason jar",
-  "Margarita glass",
-  "Martini Glass",
-  "Balloon Glass",
-  "Coupe Glass",
-];
+const { categoryTypes, glassTypes } = require("../services/constants");
 
 const schemaRegister = Joi.object({
   name: Joi.string().required(),
