c672b92b50fd548f8b0a2971f7377c7e904a7e61
Arsen Hryhoriak <136361705+Lifuss@users.noreply.github.com>
2023-12-20 17:29:13 +0200

    getFav pagination

--- controllers/drinks/favorite/getFav.js
+++ controllers/drinks/favorite/getFav.js
@@ -1,9 +1,20 @@
 const { User } = require("../../../models/user");
 
 const getFav = async (req, res) => {
+  const { page = 1, limit = 9 } = req.query;
+  const skip = (page - 1) * limit;
+  console.log(req.query);
   const { _id } = req.user;
-  const data = await User.findOne({ _id }).populate("favorites");
-  res.json(data.favorites);
+  const data = await User.findOne({ _id }, "favorites").populate(
+    "favorites",
+    "-instructionsES -instructionsDE -instructionsFR -instructionsIT -instructionsRU -instructionsPL"
+  );
+
+  res.json({
+    favorites: data.favorites.splice(skip, limit),
+    total: data.favorites.length + 1,
+    pages: Math.ceil(data.favorites.length / limit + 1),
+  });
 };
 
 module.exports = getFav;
