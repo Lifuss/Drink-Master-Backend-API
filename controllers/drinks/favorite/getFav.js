fdab9d85782cfe5dddbf227aef64723fe42a8265
kulikiliya <136320307+kulikiliya@users.noreply.github.com>
2023-12-26 16:00:40 +0200

    hot fix 2

--- controllers/drinks/favorite/getFav.js
+++ controllers/drinks/favorite/getFav.js
@@ -9,7 +9,6 @@ const getFav = async (req, res) => {
     "favorites",
     "-instructionsES -instructionsDE -instructionsFR -instructionsIT -instructionsRU -instructionsPL"
   );
-  console.log(data.favorites.length);
   res.json({
     favorites: data.favorites.splice(skip, limit),
     total: data.favorites.length + 1,
