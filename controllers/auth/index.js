6feee037bd3202e46fb42b1c2eaf78134651b154
Vitalii Dimov <113688545+Carriedbydog@users.noreply.github.com>
2023-12-26 19:50:21 +0200

    Feature/google-auth
    Created google-auth

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -1,10 +1,12 @@
 const register = require("./register");
 const login = require("./login");
 const logout = require("./logout");
+const googleAuth = require("./googleAuth");
 const { ctrlWrapper } = require("../../services");
 
 module.exports = {
   singUp: ctrlWrapper(register),
   signIn: ctrlWrapper(login),
   logout: ctrlWrapper(logout),
+  googleAuth: ctrlWrapper(googleAuth),
 };
